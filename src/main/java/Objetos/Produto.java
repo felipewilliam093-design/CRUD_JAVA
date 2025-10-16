cfc5fe8916b2d7fa76132c1368fa5acfa300762d
felipewilliam093-design <felipewilliam093@gmail.com>
2025-10-15 22:13:23 -0300

    Criação da janela produto
    Criação objeto produto + janela produto + configuração do POM adicionando dependências do MySQL.

---
+++ src/main/java/Objetos/Produto.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Objetos;
+
+
+public class Produto {
+    private int id;
+    private String descricao;
+    private double valor;
+    private int quantidade;
+
+    
+    public int getId() {
+        return id;
+    }
+
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    
+    public String getDescricao() {
+        return descricao;
+    }
+
+    
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    
+    public double getValor() {
+        return valor;
+    }
+
+   
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+   
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+    
+}
